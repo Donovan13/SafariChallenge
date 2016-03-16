5ab5236968e4c672e896fd659a9867f00e304600
Scot Robnett <scotrobnett@Scot-Robnetts-MacBook-Pro.local>
2016-03-16 08:28:11 -0500

    Latest Changes Loaded

--- SafariChallenge/ViewController.m
+++ SafariChallenge/ViewController.m
@@ -15,7 +15,10 @@
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 @property (weak, nonatomic) IBOutlet UITextField *urlTextField;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
-@property (weak, nonatomic) IBOutlet UIWebView *onBackButtonPressed;
+@property (weak, nonatomic) IBOutlet UIButton *forwardButton;
+@property (weak, nonatomic) IBOutlet UIButton *backButton;
+@property (weak, nonatomic) IBOutlet UIButton *comingSoon;
+
 
 @end
 
@@ -24,6 +27,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self loadRequestWithString: @"http://www.mobilemakers.co"];
+    
 }
 
 - (void) loadRequestWithString: (NSString *)string {
@@ -39,17 +43,73 @@
 
 - (void) webViewDidFinishLoad:(UIWebView *)webView {
     [self.spinner stopAnimating];
+    
+    if ([webView canGoBack]) {
+        [self.backButton setEnabled:YES];
+    } else {
+     [self.backButton setEnabled:NO];
+    }
+    if ([webView canGoForward]) {
+        [self.forwardButton setEnabled:YES];
+    }     else {
+        [self.forwardButton setEnabled:NO];
+    }
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)urlTextField {
-    NSURL *url = [NSURL URLWithString:urlTextField.text];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+
+//    if([self.urlTextField.text containsString:@"http://"]) {
+//        NSURL *url = [NSURL URLWithString:urlTextField.text];
+//        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+//        [self.webView loadRequest:request];
+//        return YES;
+//    } else {
+//        url =[NSString stringWithFormat:@"http://%@", urlTextField.text];
+//    }
+   
+    NSString *URLString = urlTextField.text;
+    if ([URLString rangeOfString:@"http://"].location == NSNotFound && [URLString rangeOfString:@"https://"].location == NSNotFound)
+    {
+        URLString=[NSString stringWithFormat:@"http://%@",urlTextField.text];
+    }
+    NSURL *URL = [NSURL URLWithString:URLString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
     [self.webView loadRequest:request];
-    return YES;
+    return 0;
+
 }
 
 - (IBAction)onBackButtonPressed:(UIButton *)sender {
-    self.onBackButtonPressed.enabled = self.webView.canGoBack;
+    [self.webView goBack];
+}
+- (IBAction)onForwardButtonPressed:(UIButton *)sender {
+    [self.webView goForward];
+}
+
+- (IBAction)onStopLoadingButtonPressed:(UIButton *)sender {
+    [self.webView stopLoading];
+}
+
+- (IBAction)onReloadButtonPressed:(UIButton *)sender {
+    [self.webView reload];
+}
+
+
+
+
+- (IBAction)displayTeaser:(id)sender {
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Coming Soon!" message:@"We are working day and night!!" preferredStyle:(UIAlertControllerStyleAlert)];
+    
+    UIAlertAction *Cancel = [UIAlertAction actionWithTitle: @"Go back" style:(UIAlertActionStyleCancel) handler:nil];
+  
+    [alert addAction:Cancel];
+    
+    [self presentViewController: alert animated:YES completion:nil];
 
 }
+
+
+
 @end
+
